fa4a3b6878b078e269bbd29f405329a263351848
Lubeth <lubethjimenez@yahoo.com>
2026-07-08 20:42:43 -0400

    Add history state w/ get_history function to retreive GET history data from Flask supabase orders table & display on React page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Health from './Health';
 
 function App() {
@@ -8,10 +8,40 @@ function App() {
   const [errorMessage, setErrorMessage] = useState("");
   const [history, setHistory] = useState([]);
 
+  async function get_history() {
+    try {
+      const response = await fetch(import.meta.env.VITE_BACKEND + "/api/history");
+      const data = await response.json();
+
+      setHistory(data.history);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  //Run once when the page loads//
+  useEffect(()=>{
+    get_history();
+  }, [])
+
+  //Every time history state changes, Print it to console//
+  useEffect(()=>{
+    console.log(history)
+  }, [history])
+
   return (
     <>
-      <Health />
       <h1>Week 4 Day 3 Assignment</h1>
+      <Health />
+      <h2>Order History</h2>
+      {history.map((order) =>(
+        <div key={order.id}>
+          <p>Order ID: {order.id}</p>
+          <p>Customer ID: {order.customer_id}</p>
+          <p>Status: {order.status}</p>
+          <p>Ordered At: {order.ordered_at}</p>
+        </div>
+      ))}
     </>
   )
 }
